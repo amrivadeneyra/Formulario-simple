3c7e03278303923a86f3105cf080174dcdf42454
manuel <arivadeneyrai@autonoma.edu.pe>
2023-02-13 14:42:19 -0500

    formulario simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Listado de Personas';
+
+  //dos formas de agregar datos al arreglo Persona[]
+  personas: Persona[] = [{ 'nombre': 'Manuel', 'apellido': 'Rivadeneyra' }, new Persona('Anderson', 'Isla')];
+  /*
+    nombreInput: string = '';
+    apellidoInput: string = '';
+  
+    agregarPersona() {
+      let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+      this.personas.push(persona1);
+      this.nombreInput = '';
+      this.apellidoInput = '';
+      
+    }
+  */
+
+  // **simplificando el codigo
+  agregar: any = {};
+
+  agregarPersona() {
+    this.personas.push(this.agregar);
+    this.agregar = {};
+  }
 }
